53b6dda808451c9c325b48e26e24c6ecf32b08e7
Anchungsol <ka_47@naver.com>
2019-11-29 00:22:32 +0900

    Bootstrap version 2.0

--- fakenews/news/urls.py
+++ fakenews/news/urls.py
@@ -5,5 +5,11 @@ from . import views
 urlpatterns = [
 
 	path('',views.index,name='index'),
+	path('index/',views.index),
+	path('login/',views.login,name='login'),
+	path('signup/',views.signup,name='signup'),
+	path('record/',views.record,name='record'),
+	path('searchboard/',views.searchboard,name='searchboard'),
+
 ]
 
--- fakenews/news/views.py
+++ fakenews/news/views.py
@@ -3,3 +3,15 @@ from .models import Post
 
 def index(request):
 	return render(request, 'news/index.html', {})
+
+def login(request):
+	return render(request, 'news/login.html', {})
+
+def signup(request):
+	return render(request, 'news/signup.html',{})
+
+def record(request):
+	return render(request, 'news/record.html',{})
+
+def searchboard(request):
+	return render(request, 'news/searchboard.html',{})
